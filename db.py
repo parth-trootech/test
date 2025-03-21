e8147a8e765849f382fc170b8a935fbe4533d4fc
parth.dobariya <parth.dobariya@trootech.com>
2025-03-21 18:10:33 +0530

    second commit

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel
+
 from fastapi import FastAPI
 
 DATABASE_URL = "postgresql+asyncpg://your_user:your_password@localhost/your_db_name"
@@ -13,6 +14,7 @@ async_session = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
 )
 
+
 # Function to initialize the database
 async def init_db():
     # Create all tables
@@ -20,5 +22,4 @@ async def init_db():
         # Create tables
         await conn.run_sync(SQLModel.metadata.create_all)
 
-
-#DATABASE_URL = "postgresql+asyncpg://your_user:your_password@localhost/your_db_name"
+# DATABASE_URL = "postgresql+asyncpg://your_user:your_password@localhost/your_db_name"
